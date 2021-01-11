d3e0e672b081c0e779ff9f7619d199d533899a2b
comrade77 <nimec77@gmail.com>
2021-01-11 20:58:42 +0700

    Initial commit

---
+++ app/src/main/java/ru/talkinglessons/buttoncickapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package ru.talkinglessons.buttoncickapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+const val TAG = "CounterApp"
+
+class MainActivity : AppCompatActivity() {
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: called")
+        setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button.setOnClickListener {
+            if (userInput.text.isEmpty()) {
+                return@setOnClickListener
+            }
+            textView?.append("${userInput.text}\n")
+            userInput.text.clear()
+        }
+    }
+}
